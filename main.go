e2a1b5a7b3be59504565676b5c6d9c3ef3ed71a3
Vitor Bacelar <vitor.bacelar@protonmail.com>
2022-03-29 08:40:34 -0300

    make http requests using go routines

--- main.go
+++ main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
+	start := time.Now()
+
 	links := []string{
 		"https://www.google.com",
 		"https://www.reddit.com",
@@ -14,17 +17,30 @@ func main() {
 		"https://www.stackover.com",
 	}
 
-	for _, link := range links {
+	c := make(chan string)
 
+	for _, link := range links {
+		go checkLink(link, c)
 	}
+
+	for i := 0; i < len(links); i++ {
+		fmt.Println(<- c)
+	}
+	
+	elapsed := time.Since(start)
+	fmt.Println()
+	fmt.Println()
+	fmt.Println(elapsed)
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down!")
+		c <- "Might be down, I think"
 		return
 	}
 
 	fmt.Println(link, "is up!")
+	c <- "Yep is Up"
 }
